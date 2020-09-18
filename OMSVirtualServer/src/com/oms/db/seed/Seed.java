eda0643aa2e4498c7bd2ce912576d8d22190c504
Trinh Tuan Dat <trinhtuandat.bk@gmail.com>
2020-09-18 16:00:48 +0700

    Thêm file seeds

--- OMSVirtualServer/src/com/oms/db/seed/Seed.java
+++ OMSVirtualServer/src/com/oms/db/seed/Seed.java
@@ -7,19 +7,20 @@ import java.util.ArrayList;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.oms.bean.media.Book;
 import com.oms.bean.media.Media;
 
 public class Seed {
 	private ArrayList<Media> books;
 	public void start() {
 		ObjectMapper mapper = new ObjectMapper();
-		String json = FileReader.read(getClass().getResource("./book.json").getPath());
+		String json = FileReader.read(getClass().getResource("./dvds.json").getPath());
 		try {
 			mapper.setDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
 			books = mapper.readValue(json, new TypeReference<ArrayList<Media>>() { });
 			for (Media b: books) {
 				System.out.println(b);
+				System.out.println("***");
+				System.out.println(mapper.writeValueAsString(b));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
